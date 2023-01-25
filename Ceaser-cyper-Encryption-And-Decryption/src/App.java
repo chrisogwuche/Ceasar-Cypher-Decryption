a497f6afbeb7cff01a85ebebc592e5537d94b3e2
Chris Ogwuche <ogwuchechris@gmail.com>
2023-01-25 21:50:56 +0100

    Update App.java

--- Ceaser-cyper-Encryption-And-Decryption/src/App.java
+++ Ceaser-cyper-Encryption-And-Decryption/src/App.java
@@ -4,8 +4,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         List<String> arrWord = new ArrayList<>();
-        String [] Alphabet_to_Set_Encryption = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-        String [] Alphabet_to_Set_Decryption = {"","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        List <String> Alphabet_to_Set_Encryption = Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+        List <String> Alphabet_to_Set_Decryption = Arrays.asList("","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
         List<String> arrWord_Decrypt = new ArrayList<>();
         System.out.println("!!ENCRYPT YOUR MESSAGE!!\n");
         System.out.println("ENTER MESSAGE YOU WANT TO ENCRYPT BELOW: ");
